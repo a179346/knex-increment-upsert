9d4d70231dac3236674695833f69e4c43e939bb9
hsiang <hsiang@borden.com.tw>
2021-12-15 15:10:02 +0800

    init repo

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Knex } from 'knex';
+
+export async function incrementUpsert (
+  db: Knex<any, unknown[]>,
+  tableName: string,
+  data: Record<string, any> | Record<string, any>[],
+  incrementColumns?: string[] | null,
+  updateColumns?: string[] | null
+): Promise<number[]> {
+  const mergeData: Record<string, Knex.Raw<any>> = {};
+  if (incrementColumns) {
+    for (const colName of incrementColumns) {
+      mergeData[colName] = db.raw('?? + values(??)', [ colName, colName ]);
+    }
+  }
+  if (updateColumns) {
+    for (const colName of updateColumns) {
+      mergeData[colName] = db.raw('values(??)', [ colName ]);
+    }
+  }
+
+  const result = await db(tableName)
+    .insert(data)
+    .onConflict()
+    .merge(mergeData);
+  return result;
+}
